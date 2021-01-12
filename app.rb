bf87fca73deae6397ce35be1cdb80193f8e1b59f
James Sapp <jameseisapp@gmail.com>
2021-01-12 14:18:57 -0600

    commit tree 632ed18aae1bda732ab8c2f22b2383a8e64013a6

--- app.rb
+++ app.rb
@@ -3,7 +3,13 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :root  
+    end
+
+    get '/new' do
+      erb :new  
+    end
 
   end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,23 @@
 class Ship
+
+    attr_reader :name, :type, :booty
+
+    SHIPS = []
+
+    def initialize(args)
+        @name = args[:name]
+        @type = args[:type]
+        @booty = params[:booty]
+        @@all << self 
+    end
+
+    def self.all
+        SHIPS
+    end
+
+    def self.clear
+        self.all.delete 
+    end
+
+
 end
