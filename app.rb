7aecd68c8e96f2bdb0959dad4292dbc4dc8619c3
stundennull <stundennull@gmail.com>
2021-01-12 15:24:41 -0600

    commit tree a13c44fbb7996f50cacb6e7dfea32fd55b41b6af

--- app.rb
+++ app.rb
@@ -2,14 +2,23 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
+    set :views, "views/pirates"
+    set :method_override, true
 
     get '/' do
       erb :root  
     end
 
-    get '/new' do
-      erb :new  
+    get '/pirates/new' do
+
+      erb :new
     end
+
+    post '/pirates/new' do 
+      erb :show
+    end
+
+
 
   end
 end
